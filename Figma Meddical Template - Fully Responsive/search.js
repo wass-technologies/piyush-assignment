74cc03aa2bb49b296553dccd9cfdb16e32503236
Piyush Chandra Mishra <cse2118.diatm@gmail.com>
2025-03-15 12:16:51 +0530

    Delete Figma Meddical Template - Fully Responsive directory

--- Figma Meddical Template - Fully Responsive/search.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("search-btn").addEventListener("click", function() {
-    let query = document.getElementById("search-input").value;
-    if (query) {
-        alert("Searching for: " + query);
-    } else {
-        alert("Please enter a search query!");
-    }
-});
